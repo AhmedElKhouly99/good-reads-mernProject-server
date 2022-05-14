e93e5658a9ac4a13ef86c0c2de0e60c206232ff5
Ahmed ElKhouly <ahmed.elsayed.elkhouly99@gmail.com>
2022-05-14 15:56:31 +0200

    popular authors

--- routers/books/bookRouter.js
+++ routers/books/bookRouter.js
@@ -3,65 +3,78 @@ const express = require('express');
 var bodyParser = require('body-parser');
 // const jwt = require('jsonwebtoken');
 // const signAsync = util.promisify(jwt.sign);
-const {customError, authError} = require('../../helpers/customErrors');
+const { customError, authError } = require('../../helpers/customErrors');
 const BookModel = require('./bookModel');
 
 const bookRouter = express.Router();
 var cors = require('cors');
-bookRouter.use(bodyParser.json({limit: "50mb"}));
-bookRouter.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+bookRouter.use(bodyParser.json({ limit: "50mb" }));
+bookRouter.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 const { authorizeUser, authorizeAdmin } = require('../../helpers/middlewares');
 bookRouter.use(cors())
-bookRouter.use((req,res, next)=> {
+bookRouter.use((req, res, next) => {
     console.log(req.url);
     next();
 });
 
 bookRouter.post('/', async (req, res, next) => {
-    const { name,AuthorId,CategoryId,image} = req.body;
-    
+    const { name, AuthorId, CategoryId, image } = req.body;
+
     try {
-        
-        await BookModel.create({ name,AuthorId,CategoryId,image});
-        res.send({success: true});
+
+        await BookModel.create({ name, AuthorId, CategoryId, image });
+        res.send({ success: true });
     } catch (error) {
         next(error);
     }
 });
 
-bookRouter.get('/', async (req, res, next)=> {
+bookRouter.get('/', async (req, res, next) => {
 
     try {
-      
+
         BookModel.aggregate([{
             $lookup: {
-                    from: "authors",
-                    localField: "AuthorId",
-                    foreignField: "_id",
-                    as: "author"
-                }
-        }, 
+                from: "authors",
+                localField: "AuthorId",
+                foreignField: "_id",
+                as: "author"
+            }
+        },
         {
             $lookup: {
-                    from: "categories",
-                    localField: "CategoryId",
-                    foreignField: "_id",
-                    as: "category"
-                }
+                from: "categories",
+                localField: "CategoryId",
+                foreignField: "_id",
+                as: "category"
+            }
         }
-    ],function (error, data) {
+        ], function (error, data) {
             return res.send(data);
-       
-   })
-     
+
+        })
+
     } catch (error) {
         next(error);
     }
-    
+
 });
 
-bookRouter.get('/:id', async (req, res, next)=> {
+bookRouter.get("/popular", async (req, res, next) => {
+    try {
+        console.log("hjklkjhjk");
+        // .sort({name: -1})
+        const popularBooks = await BookModel.find().sort({name: -1});
+        res.send({popularBooks}); 
+
+        res.status(200).send({ message: "successfully deleted" });
+    } catch (error) {
+        next(error);
+    }
+});
+
+bookRouter.get('/:id', async (req, res, next) => {
     const { id } = req.params;
 
     try {
@@ -70,15 +83,15 @@ bookRouter.get('/:id', async (req, res, next)=> {
     } catch (error) {
         next(error);
     }
-    
+
 });
 
-bookRouter.patch('/:id' ,async (req, res,next)=> {
+bookRouter.patch('/:id', async (req, res, next) => {
 
     const { id } = req.params;
     try {
-        await BookModel.findByIdAndUpdate(id, {$set: req.body});
-        res.send({message: 'updated successfully'}); 
+        await BookModel.findByIdAndUpdate(id, { $set: req.body });
+        res.send({ message: 'updated successfully' });
     } catch (error) {
         next(error);
     }
@@ -89,24 +102,15 @@ bookRouter.delete("/:id", async (req, res, next) => {
     const { id } = req.params;
     console.log(id)
     try {
-      await BookModel.findByIdAndDelete(id);
-      res.send({ message: "successfully deleted" });
-  
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  bookRouter.get("popular",  async (req, res, next)=> {
-    try {
-        // .sort({name: -1})
-        // const popularBooks = await BookModel.find();
-        // res.send({popularBooks}); 
+        await BookModel.findByIdAndDelete(id);
         res.send({ message: "successfully deleted" });
-      } catch (error) {
+
+    } catch (error) {
         next(error);
-      }
-  });
+    }
+});
+
+
 
 
 module.exports = bookRouter;
