6d81789ffab036e82d24325b1d788cd476f87288
Abanoub <abanoub.kamal1998@gmail.com>
2022-05-15 06:55:38 +0200

    popular things

--- routers/books/bookRouter.js
+++ routers/books/bookRouter.js
@@ -9,6 +9,7 @@ const BookModel = require('./bookModel');
 const bookRouter = express.Router();
 var cors = require('cors');
 const { authorizeUser, authorizeAdmin } = require('../../helpers/middlewares');
+
 bookRouter.use(cors())
 bookRouter.use((req,res, next)=> {
     console.log(req.url);
@@ -19,7 +20,6 @@ bookRouter.post('/', async (req, res, next) => {
     const { name,CategoryId,AuthorId} = req.body;
     // const { token } = req.headers;
     try {
-        
         await BookModel.create({ name,CategoryId,AuthorId});
         res.send({success: true});
     } catch (error) {
@@ -57,6 +57,39 @@ bookRouter.get('/', async (req, res, next)=> {
     }
     
 });
+
+bookRouter.patch('/rate', async (req, res, next) => {
+    const {rating, bookId} = req.body;
+    try {
+        // await BookModel.findByIdAndUpdate(bookId, {$set: {noOfRatings:noOfRatings++ , rating: ()}});
+        res.send({message: 'updated successfully'}); 
+    } catch (error) {
+        next(error);
+    }
+})
+
+bookRouter.get("/popular", async (req, res, next) =>{
+    try {
+        // let popularBooks = await BookModel.aggregate([
+        //     { $group : { _id : {}, books: { $push: "$name" } } },
+        //     { $project: {
+        //         _id: 1,
+        //         numberOfBooks: { $cond: { if: { $isArray: "$books" }, then: { $size: "$books" }, else: "NA"} }
+        //      } },
+        //     { $sort  : { numberOfBooks: 1 }}
+        //     // { $slice: [ "$numberOfBooks", 3 ] }
+        // ])
+
+        const popularBooks = await (await BookModel.find({}).sort({_id: 1})).splice(3);
+
+        // popularBooks = popularBooks.slice(-3).reverse();
+
+        res.status(200).send(popularBooks);
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 bookRouter.get('/:id', async (req, res, next)=> {
     const { id } = req.params;
--- routers/books/bookSchema.js
+++ routers/books/bookSchema.js
@@ -1,7 +1,10 @@
+const { number } = require('joi');
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
     // photo:'string,'
+    rating: 'number',
+    noOfRatings: 'number',
     name: 'string',
     CategoryId:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
     AuthorId:[{ type: mongoose.Schema.Types.ObjectId,ref:'Author' }]
