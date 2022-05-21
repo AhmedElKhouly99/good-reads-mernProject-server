af48f518249d7f7dd0d4aaf5d4caba2e4cfec3fb
Ahmed ElKhouly <ahmed.elsayed.elkhouly99@gmail.com>
2022-05-21 21:51:21 +0200

    update

--- routers/books/bookRouter.js
+++ routers/books/bookRouter.js
@@ -133,7 +133,6 @@ bookRouter.patch('/', async (req, res, next) => {
         const id = await getIdFromToken(token);
 console.log(req);
         if (isRated && (rating || rating == 0)) {
-            // await BookModel.findByIdAndUpdate(Bid, { $inc: { rating: rating - oldRating } });
             await BookModel.findByIdAndUpdate(Bid, { $inc: { noOfRatings: 1, rating: rating } });
             await UsersModel.updateOne(
                 { '_id': id, "books": { $elemMatch: { _id: Bid } } },
@@ -151,7 +150,6 @@ console.log(req);
             await UsersModel.updateOne(
                 { '_id': id, "books._id": Bid },
                 { '$set': { 'books.$.rating': rating, 'books.$.isRated': isRated, 'books.$.status': status, 'books.$.review': review } })
-            // await BookModel.findByIdAndUpdate(Bid, { $inc: { noOfRatings: 1, rating: rating } });
             await BookModel.findByIdAndUpdate(Bid, { $inc: { rating: rating - oldRating } });
         }
 
